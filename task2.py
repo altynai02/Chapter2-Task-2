18edeb41b2ba4a1dce1c5b0c053b28e3857679ea
altynai <altynay.bakytbekova.02@gmal.com>
2020-03-11 21:35:08 +0600

    added

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
 # 2) Попросить пользователя ввести слова через пробел. Отсортировать слова по количеству
 # символов и вывести на экран результат.
-
+str_text = " " # обратно в строку с пробелом
 your_text = input("Please write your text with space: ")
 text = your_text.split()
 text.sort(key = len) # by lenght
-print(text)
+print(str_text.join(text))
